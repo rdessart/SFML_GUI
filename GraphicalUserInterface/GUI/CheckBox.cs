750e5d185c0e7de82c5310b0142768276dd918e8
Romain DESSART <romaindessart@gmail.com>
2021-01-22 09:32:35 +0100

    Creating and implenting checkboxes

---
+++ GraphicalUserInterface/GUI/CheckBox.cs
@@ -0,0 +1,38 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicalUserInterface.GUI
+{
+    public class CheckBox : Element
+    {
+        public CheckBox() : base()
+        {
+            Background = new Color(0x73, 0x73, 0x73);
+            this.Update();
+        }
+
+        public override void Update()
+        {
+            _text.Clear(Color.Transparent);
+            RectangleShape rectange = new RectangleShape(
+                new Vector2f(25.0f, 25.0f)
+                - new Vector2f(2.0f * Border.BorderThickness, 2.0f * Border.BorderThickness))
+            {
+                Position = new Vector2f(Border.BorderThickness, Border.BorderThickness),
+                OutlineThickness = Border.BorderThickness,
+                OutlineColor = Border.BorderColor,
+                FillColor = State ? Background + new Color(0x50, 0x50, 0x50) : Background,
+            };
+            _text.Draw(rectange, RenderStates.Default);
+            _text.Display();
+            _sprite = new Sprite(_text.Texture)
+            {
+                Position = Position
+            };
+            _rect = _sprite.GetLocalBounds();
+        }
+    }
+}
--- GraphicalUserInterface/Program.cs
+++ GraphicalUserInterface/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using GraphicalUserInterface.GUI;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
 
 namespace GraphicalUserInterface
 {
     class Program
     {
+        public static List<Element> boxes;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            boxes = new List<Element>()
+            {
+                new CheckBox(),
+            };
+            Font f = new Font(@"C:\Windows\Fonts\arial.ttf");
+            Text t = new Text("Hello World", f, 25);
+            RenderWindow win = new RenderWindow(new VideoMode(1000, 1000), "Hello World", Styles.Default);
+            win.Closed += (o, e) => win.Close();
+            win.MouseButtonPressed += Win_MouseButtonPressed;
+            while (win.IsOpen)
+            {
+                win.DispatchEvents();
+                win.Clear(Color.Black);
+                foreach(CheckBox cb in boxes)
+                {
+                    cb.Draw(win);
+                }
+                win.Display();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Win_MouseButtonPressed(object sender, MouseButtonEventArgs e)
+        {
+            foreach (Element elem in boxes)
+            {
+                if(elem.Clicked(new Vector2f(e.X, e.Y)))
+                {
+                    elem.State = !elem.State;
+                    elem.Update();
+                    break;
+                }
+            }
         }
     }
 }
