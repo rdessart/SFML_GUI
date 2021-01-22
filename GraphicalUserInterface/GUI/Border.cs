a028a9c2455946d9089d80f346ce19814bcdf6f2
Romain DESSART <romaindessart@gmail.com>
2021-01-22 09:32:20 +0100

    Created Border class

---
+++ GraphicalUserInterface/GUI/Border.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+using System.Text;
+
+namespace GraphicalUserInterface.GUI
+{
+    public class Border
+    {
+        public Color BorderColor { get; set; }
+        public uint BorderThickness { get; set; }
+
+        public Border()
+        {
+            BorderThickness = 1;
+            BorderColor = Color.Black;
+        }
+    }
+}
